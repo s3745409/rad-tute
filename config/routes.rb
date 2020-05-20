4f063f4964adad8d9f0c36b79cf2e5937655317a
Daniel Dominique <danmeldominique@gmail.com>
2020-05-20 16:29:49 +1000

    Week 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options[:host] = "localhost:3000"
+
 Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
