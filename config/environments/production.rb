13067093286056ebf51f394ca716c8f97f910dfe
Daniel Dominique <danmeldominique@gmail.com>
2020-05-21 17:04:03 +1000

    Updated mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'rad-tute-app.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rad-tutorials.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
